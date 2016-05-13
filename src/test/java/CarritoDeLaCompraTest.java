86043751586e7dfd32a3ee8d6c3c3727e41a3613
guillermo <gmuela92@gmail.com>
2016-05-13 15:42:19 +0200

    Test olvidados añadidos

--- src/test/java/CarritoDeLaCompraTest.java
+++ src/test/java/CarritoDeLaCompraTest.java
@@ -5,7 +5,68 @@ import org.junit.Test;
 
 public class CarritoDeLaCompraTest {
 
+    @Test
+    public void shouldReturnTheCountOfAllItems() throws Exception {
 
+        CarritoBuilder builder = new CarritoBuilder(30L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(30, carritoDeLaCompra.contarNumeroProductos());
+    }
+
+    @Test
+    public void shouldCalculateTotalPrice() throws Exception {
+
+        CarritoBuilder builder = new CarritoBuilder(60L,5L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(300L, carritoDeLaCompra.calcularPrecioTotal());
+
+    }
+
+    @Test
+    public void shouldCalculateTotalPriceLambda() throws Exception {
+
+        CarritoBuilder builder = new CarritoBuilder(60L, 5L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(300L, carritoDeLaCompra.calcularPrecioTotalLambda());
+
+    }
+
+    @Test
+    public void shouldCalculateTotalPriceRefMethod() throws Exception {
+
+        CarritoBuilder builder = new CarritoBuilder(60L,5L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(300L, carritoDeLaCompra.calcularPrecioTotalRefMethod());
+
+    }
+
+    @Test
+    public void shouldCalculateTotalDiscount() throws Exception {
+
+        CarritoBuilder builder = new CarritoBuilder(20L,100L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(100L, carritoDeLaCompra.calcularDescuentoTotal(100));
+
+    }
+
+    @Test
+    public void shouldCalculateTotalDiscountLambda() throws Exception {
+
+        CarritoBuilder builder = new CarritoBuilder(20L,100L);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(100, carritoDeLaCompra.calcularDescuentoTotalLambda(100));
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldDetectErrorAnThrowRuntimeExceptionWhenAPriceIsNull(){
+
+        CarritoBuilder builder = new CarritoBuilder(20L,100L);
+        builder.add(null);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        carritoDeLaCompra.detectarError();
+
+    }
 
     @Test(expected = RuntimeException.class)
     public void shouldDetectErrorAnThrowRuntimeExceptionWhenAPriceIsNullAnyMatch(){
